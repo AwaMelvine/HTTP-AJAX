5cf8dea00edfdf7a558cf228fdfc7cf9fa669070
Awa Melvine <melvineawa@gmail.com>
2019-06-12 13:50:37 +0100

    Fetch friends data from endpoint in app component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+import FriendList from "./components/Friends/FriendList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>App</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      friends: []
+    };
+  }
+  async componentDidMount() {
+    const friendsData = await axios.get("http://localhost:5000/friends");
+    this.setState({ friends: friendsData.data });
+  }
+  render() {
+    const { friends } = this.state;
+    return (
+      <div className="App">
+        <FriendList friends={friends} />
+      </div>
+    );
+  }
 }
 
 export default App;
